b0bb53df3ae6dfa0147852ce1855c3ce7a265c19
Rafa6tz <rafa6tz@gmail.com>
2025-03-23 09:26:19 -0300

    starting project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <>
+      <header className="w-full flex justify-center items-center h-20 bg-slate-400">
+        <h2 className="font-bold text-2xl">Todo Typescript</h2>
+      </header>
+      
+    </>
+  )
+}
+
+export default App
