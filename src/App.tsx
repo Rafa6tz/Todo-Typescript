50dbd6a92eeeb32b06d1ba03f1647e9bbe781064
Rafa6tz <rafa6tz@gmail.com>
2025-03-23 10:47:51 -0300

    Doing the form

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
+import { useState } from "react"
+import TaskForm from "./components/TaskForm"
+
+//interface
+import { ITask } from "./interfaces/Task"
 
 function App() {
+
+  const [ taskList, setTaskList ] = useState<ITask[]>([])
 
   return (
     <>
       <header className="w-full flex justify-center items-center h-20 bg-slate-400">
         <h2 className="font-bold text-2xl">Todo Typescript</h2>
       </header>
-      
+      <TaskForm btnText="Cadastrar tarefa" taskList={taskList} setTaskList={setTaskList}/>
     </>
   )
 }
---
+++ src/components/TaskForm.tsx
@@ -0,0 +1,61 @@
+import React, { ChangeEvent, FormEvent, useState } from 'react'
+
+//interface
+import { ITask } from '../interfaces/Task'
+
+type Props = {
+    btnText: string,
+    setTaskList?: React.Dispatch<React.SetStateAction<ITask[]>>,
+    taskList: ITask[]
+}
+
+const TaskForm = ({btnText, setTaskList, taskList}: Props) => {
+
+    const [id, setId] = useState<number>(0)
+    const [title, setTitle] = useState<string>('')
+    const [description, setDescription] = useState<string>('')
+
+
+    // Atribui o resultado do input ao mudar
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        if(e.target.name === 'title') {
+            setTitle(e.target.value)
+        } else {
+            setDescription(e.target.value)
+        }
+    }
+
+    // Adiciona ou Atualiza tarefa
+    const addTaskHandle = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        // cria um id aleatório
+        const id = Math.floor(Math.random() * 1000)
+
+        const newTask: ITask = {id, title, description}
+
+        setTaskList!([...taskList, newTask])
+        setTitle('')
+        setDescription('')
+
+        console.log(taskList)
+    }
+
+  return (
+    <div className='w-full flex justify-center items-center h-52 my-4'>
+    <form onSubmit={addTaskHandle} className='flex justify-center flex-col items-center w-full'>
+        <div className='py-2 flex justify-center flex-col items-center w-full'>
+            <label className='font-semibold text-lg' htmlFor='title'>Título da tarefa: </label>
+            <input onChange={handleChange} value={title} id='title' name='title' type='text' className='border border-slate-600 rounded-lg w-3/5 p-1.5 md:w-2/5' />
+        </div>
+        <div className='py-2 flex justify-center flex-col items-center w-full'>
+            <label className='font-semibold text-lg' htmlFor='description'>Descrição: </label>
+            <input onChange={handleChange} value={description} id='description' name='description' type='text' className='border border-slate-600 rounded-lg w-3/5 p-1.5 md:w-2/5' />
+        </div>
+        <input type='submit' value={btnText} className='bg-slate-400 border border-slate-700 w-46 rounded-3xl h-10 my-2 text-lg font-bold cursor-pointer hover:text-white'/>
+    </form>
+    </div>
+  )
+}
+
+export default TaskForm
